d36249713d94472cd5553454e3b5497a915d9dfd
murali-mohan <muralimohan.sampath@outlook.com>
2025-08-09 01:01:52 -0400

    api-gateway trip/preview handler

---
+++ services/api-gateway/http.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"ride-sharing/shared/contracts"
+)
+
+func handleTripPreview(w http.ResponseWriter, r *http.Request) {
+
+	var reqBody preveiwTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	//validation
+
+	if reqBody.UserID == "" {
+		http.Error(w, "user ID is required", http.StatusBadRequest)
+		return
+	}
+
+	//TODO call trip service
+
+	response := contracts.APIResponse{Data: "ok"}
+
+	writeJSON(w, http.StatusCreated, response)
+
+}
